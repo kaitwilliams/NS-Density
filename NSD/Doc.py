9193a8f11d51d97de23f482f5e8b8ffea7fb23b2
Kaitlin Williams <kwilli18@mail.uoguelph.ca>
2018-01-03 11:41:36 -0500

    Cleaned Dwarf code to now run relative and non-relative alpha and beta inserts. Next step: get alpha and beta running through other constants (g, c, R0, etc.).

---
+++ NSD/Doc.py
@@ -0,0 +1,111 @@
+# NEWTONIAN RELATIVISTIC WHITE DWARF
+# SOLAR MASSES & UNITLESS
+# Kaitlin Williams
+#
+#
+#
+import numpy as np
+import math
+from matplotlib.pyplot import *
+
+#seterr(all='print')
+
+g=6.673*(10**-8)
+c=299792458
+#solarmass =
+powerstuff = 4./3
+e0= 0.01392
+R0 = 1.473
+K = (1./4.173)**(powerstuff-1)
+
+p0 = [10**-16]
+#p0 = np.arange(0.001,1.00,0.001,object)
+
+
+#-------------------------------
+
+#alpha = R0/((K*(e0**(powerstuff-1))))**(1/powerstuff)
+#print alpha
+#alpha = 0.05
+#beta = 0.005924
+
+alpha = 1.473
+beta = 52.46
+
+#beta = (4*math.pi*e0)/((c**2)*(K*(e0**(powerstuff-1)))**(1/powerstuff))
+# ^^ needs the solar mass term otherwise waaaay too small
+#print beta
+#-------------------------------
+
+
+
+def diffpressure(p, r, m):
+     if m ==0:
+        return p
+     if p <= 0 :
+        print "I'M NEGATIVE  ", p
+        return -1
+     else:
+        return -(alpha*m*(p**(1./powerstuff)))/(r**2)
+#    return -(g*epsilon(r)*mass(r))/((r*c)**2)
+
+def diffmass(p, r):
+    if p <= 0 :
+        print "I'M NEGATIVE ", p
+        return -1
+    return (p**(1./powerstuff))*beta*(r**2)
+
+
+
+
+def solve_rk4_coupled(mass, pressure, p0, m0, N, rinitial, rfinal):
+    p = p0
+    m = m0
+    r = rinitial
+    h = (rfinal - rinitial) / float(N)
+    data = [[r, p, m]]
+    for i in range(1,N):
+        if i % 1000 == 0:
+            print ".",
+        l1 = h * mass(p, r)
+        k1 = h * pressure(p, r, m)
+        l2 = h * mass(p + k1 / 2., r + h / 2.)
+        k2 = h * pressure(p + k1 / 2., r + h / 2., m + l1 / 2.)
+        l3 = h * mass(p + k2 / 2., r + h / 2.)
+        k3 = h * pressure(p + k2 / 2., r + h / 2., m + l2 / 2.)
+        l4 = h * mass(p + k3, r + h)
+        k4 = h * pressure(p + k3, r + h, m + l3)
+        r += h
+        p += (k1 + (2 * k2) + (2 * k3) + k4)/6.
+        m += (l1 + (2 * l2) + (2 * l3) + l4)/6.
+        if p <= 0:
+            break
+        data.append([r, p, m])
+    else:
+        print data[-1]
+        data = None
+
+    return data
+
+finalrs = []
+finalms = []
+
+
+for press in p0:
+    stardata = solve_rk4_coupled(diffmass, diffpressure, press, 0., 10000001, 0, 20000.)
+    rs = []
+    ms = []
+    ps = []
+
+    for elem in stardata:
+        rs.append(elem[0])
+        ps.append(elem[1])
+        ms.append(elem[2])
+    finalrs.append(stardata[-1][0])
+    finalms.append(stardata[-1][2])
+    print stardata[-1]
+    plot(rs, ps, 'r')
+    #plot(rs, ms, 'b')
+
+plot(finalrs, finalms, 'r')
+show()
